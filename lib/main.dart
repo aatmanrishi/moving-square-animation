ca8c759d98488932b6ff69450ac4af67b49e8df7
Rishabh Shukla <116421912+aatmanrishi@users.noreply.github.com>
2024-11-09 10:49:51 +0530

    second-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,50 @@
+/// Import necessary packages
 import 'package:animation_moving_square/Controller/mainController.dart';
 import 'package:animation_moving_square/Pages/Home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+/// Entry point of the application.
 void main() {
-  Get.put(MainController());
-  runApp(const MyApp());
+// Initialize the MainController using GetX
+Get.put(MainController());
+
+// Run the MyApp widget
+runApp(const MyApp());
 }
 
+/// Root widget of the application.
+///
+/// This widget is responsible for setting up the app's theme and routing.
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// Constructor for MyApp widget.
+const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Home());
-  }
+// This widget is the root of your application.
+@override
+/// Builds the MyApp widget.
+///
+/// Returns a GetMaterialApp widget with customized theme and routing.
+Widget build(BuildContext context) {
+return GetMaterialApp(
+// Set app title
+title: 'Flutter Demo',
+
+  // Hide debug banner
+  debugShowCheckedModeBanner: false,
+  
+  // Customize app theme
+  theme: ThemeData(
+    // Set color scheme
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    
+    // Enable Material 3 design
+    useMaterial3: true,
+  ),
+  
+  // Set home page
+  home: const Home(),
+);
+
+}
 }
